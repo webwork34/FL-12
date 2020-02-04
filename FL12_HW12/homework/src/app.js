ecc3d314491847314a41f7e025e16797b6588713
Kostiantyn Marych <54593230+webwork34@users.noreply.github.com>
2020-02-04 14:19:56 +0200

    completed task

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -5,17 +5,64 @@ const addBtn = document.getElementById('add_new_set_btn'),
       addTerms = document.getElementById('add_terms'),
       saveTerms = document.getElementById('save_terms'),
       cancelBtn = document.getElementById('cancel'),
-      inputName = document.getElementById('input_name');
+      inputName = document.getElementById('input_name'),
+      openTermsBox = document.getElementById('open_terms'),
+      studiedTermsBox = document.getElementById('studied_terms');
+
 const zero = 0;
-let count, setsArr;
+let count, setsArr, 
+    studied = []; 
 
 
-// function checkHash(){
-//   console.log(document.location.hash);
-// }
-// checkHash();
+// відображення вивченних сетів
+function renderStudied(){
+  if(localStorage.getItem('studied')){
+    studied = JSON.parse(localStorage.getItem('studied'));
+
+    for (let i = 0; i < studied.length; i++) {
+      const divSets = document.createElement('div');
+      studiedTermsBox.appendChild(divSets).classList.add('studied_block');
+      divSets.textContent = studied[i].name;
+
+      const editBtn = document.createElement('button');
+      const removeBtn = document.createElement('button');
+
+      divSets.appendChild(editBtn).classList.add('edit_set_btn_studied');
+      divSets.appendChild(removeBtn).classList.add('remove_set_btn_studied');
+
+      editBtn.textContent = 'Edit';
+      removeBtn.textContent = 'Remove';
+
+      editBtn.disabled = true;
+      const removeStudied = studiedTermsBox.querySelectorAll('.remove_set_btn_studied');
+      const spanStudied = document.createElement('span');
+      divSets.appendChild(spanStudied);
+
+      let allSpanStudied = studiedTermsBox.querySelectorAll('span');
+      
+      // видалення вивчених сетів
+      removeStudied.forEach((btn, index) => {
+        allSpanStudied[index].textContent = index;
+        btn.onclick = function(){
+          let indx = parseInt(this.nextSibling.textContent);
+          studied.splice(indx, 1);
+          this.closest('.studied_block').remove();
+          allSpanStudied = studiedTermsBox.querySelectorAll('span');
+  
+          for (let i = indx; i < studied.length; i++) {
+            allSpanStudied[i].textContent = i;
+          }
+          localStorage.setItem('studied', JSON.stringify(studied));
+        };
+      });
+      // кінець видалення вивчених сетів
+    }
+  }
+}
+// кінець відображення вивченних сетів
 
 
+// перевірка LS та відображення сетів
 function checkLS(){
   history.replaceState(null, null, ' ');
   if(localStorage.getItem('idTermsLS')){
@@ -29,7 +76,7 @@ function checkLS(){
   
     for (let i = 0; i < setsArr.length; i++) {
       const divSets = document.createElement('div');
-      mainPage.appendChild(divSets).classList.add('block');
+      openTermsBox.appendChild(divSets).classList.add('block');
       divSets.textContent = setsArr[i].name;
 
       const editBtn = document.createElement('button');
@@ -41,59 +88,192 @@ function checkLS(){
       editBtn.textContent = 'Edit';
       removeBtn.textContent = 'Remove';
 
-      const removeSets = document.querySelectorAll('.remove_set_btn');
-      const editSets = document.querySelectorAll('.edit_set_btn');
-
-      removeSets.forEach((btn, index) => {
-        btn.addEventListener('click', function(){
-          this.closest('.block').remove();
-          setsArr.splice(index, 1);
-          index--;
-          localStorage.setItem('setsOfWords', JSON.stringify(setsArr));
-        });
-      });
+      const span = document.createElement('span');
+      divSets.appendChild(span);
     }
+
+    const blocks = openTermsBox.querySelectorAll('.block');
+    let allSpan = openTermsBox.querySelectorAll('span');
+
+    // помітити сет вивченим
+    blocks.forEach(block => {
+      block.addEventListener('click', function(){
+
+        let indx = parseInt(this.lastElementChild.textContent);
+        setsArr[indx].studied = true;
+        let studiedSet = setsArr.splice(indx, 1);
+        studied.push(...studiedSet);
+        this.remove();
+        allSpan = openTermsBox.querySelectorAll('span');
+
+        for (let i = indx; i < setsArr.length; i++) {
+          allSpan[i].textContent = i;
+        }
+
+        localStorage.setItem('studied', JSON.stringify(studied));
+        localStorage.setItem('setsOfWords', JSON.stringify(setsArr));
+
+        const studiedBlocks = studiedTermsBox.querySelectorAll('.studied_block');
+        studiedBlocks.forEach(block => {
+          block.remove();
+        });
+        // кінець помітити сет вивченим
+
+        renderStudied();
+      });
+    });
+
+    const removeSets = openTermsBox.querySelectorAll('.remove_set_btn');
+    let editSets = openTermsBox.querySelectorAll('.edit_set_btn');
+    
+    //видалення сетів
+    removeSets.forEach((btn, index) => {
+      allSpan[index].textContent = index;
+      btn.addEventListener('click', function(e){
+        e.stopPropagation();
+        let indx = parseInt(this.nextSibling.textContent);
+        console.log(setsArr);
+        setsArr.splice(indx, 1);
+        console.log(setsArr);
+        this.closest('.block').remove();
+        allSpan = openTermsBox.querySelectorAll('span');
+
+        for (let i = indx; i < setsArr.length; i++) {
+          allSpan[i].textContent = i;
+        }
+        localStorage.setItem('setsOfWords', JSON.stringify(setsArr));
+      });
+    });
+    //кінець видалення сетів
+
+    
+    editSets.forEach(btn => {
+      btn.addEventListener('click', function(e){
+        e.stopPropagation();
+        let indx = parseInt(this.nextSibling.nextSibling.textContent);
+        mainPage.style.display = 'none';
+        editPage.style.display = 'block';
+        window.location.hash = `#/modify:item_${setsArr[indx].id}`;
+      });
+    });
+    
   }else{
     setsArr = [];
   }
 }
+// кінець перевірка LS та відображення сетів
+
+
+//редагування сетів
+function modify(){
+  let indxId = +window.location.hash.replace('#/modify:item_', '');
+  let indx;
+
+  for (let i = 0; i < setsArr.length; i++) {
+    if(indxId === +setsArr[i].id){
+      indx = i;
+    }
+  }
+
+  const editName = document.createElement('input'),
+        cancel = document.createElement('button'),
+        saveChanges = document.createElement('button');
+
+  editPage.appendChild(editName);
+  editName.style.display = 'block';
+  editName.value = setsArr[indx].name;
+
+  editPage.appendChild(saveChanges);
+  editPage.appendChild(cancel).id = 'editCancel';
+  saveChanges.textContent = 'Save changes';
+  cancel.textContent = 'Cancel';
+
+  const editCancel = document.getElementById('editCancel');
+
+  // відміна редагування
+  editCancel.addEventListener('click', function(){
+    this.previousSibling.previousSibling.remove();
+    this.previousSibling.remove();
+    this.remove();
+    exit();
+  });
+  // кінець відміна редагування
+
+  // відображення карток редагуючого сету
+  for (let j = 0; j < setsArr[indx].arrOfTerms.length; j++) {
+    const editBlock = document.createElement('div');
+    
+    editPage.appendChild(editBlock).classList.add('block');
+    const editTermCreate = document.createElement('input'),
+          editDefenitionCreate = document.createElement('input');
+          
+    editBlock.appendChild(editTermCreate).classList.add('edit_term');
+    editBlock.appendChild(editDefenitionCreate).classList.add('edit_defenition');
+    
+    editTermCreate.value = setsArr[indx].arrOfTerms[j].term;
+    editDefenitionCreate.value = setsArr[indx].arrOfTerms[j].defenition;
+    // кінець відображення карток редагуючого сету
+
+    // збереження відредагованих сетів
+    saveChanges.onclick = function(){
+      if(editName.value !== ''){
+
+        const editTermQuery = document.querySelectorAll('.edit_term'),
+              editDefenetionQuery = document.querySelectorAll('.edit_defenition');
+
+        for (let i = 0; i < editTermQuery.length; i++) {
+          setsArr[indx].arrOfTerms[i].term = editTermQuery[i].value;
+          setsArr[indx].arrOfTerms[i].defenition = editDefenetionQuery[i].value;
+        }
+
+        setsArr[indx].name = editName.value;
+        localStorage.setItem('setsOfWords', JSON.stringify(setsArr));
+        this.previousSibling.remove();
+        this.nextSibling.remove();
+        this.remove();
+        exit();
+      }
+    };
+    //кінець збереження відредагованих сетів
+  }
+}
+//кінець редагування сетів
+
 
 function exit(){
-  mainPage.style.display = 'block';
-  addPage.style.display = 'none';
+  inputName.value = '';
   const blocks = document.querySelectorAll('.block');
   blocks.forEach(block => {
     block.remove();
   });
-  inputName.value = '';
+  addPage.style.display = 'none';
+  editPage.style.display = 'none';
+  mainPage.style.display = 'block';
   checkLS();
 }
 
-function addNewSet(){
-  addBtn.addEventListener('click', function(){
-    window.location.hash = '#/add';
-    count++;
-    localStorage.setItem('idTermsLS', count);
-    mainPage.style.display = 'none';
-    addPage.style.display = 'block';
-    // window.addEventListener('hashchange', function(){
-      
-      // checkHash();
-    // });
-    // history.replaceState(null, null, ' ');  // очистить hash
-  
-    // window.onhashchange = function(e){
-    //   console.log(e); // для себя просмотрите объект
-    //   // alert('Хеш сейчас ' + document.location.hash)
-    // }
-    cancelBtn.onclick = function(){
-      count--;
-      localStorage.setItem('idTermsLS', count);
-      exit();
-    }
-  });
-}
 
+// додати новий сет
+function addNewSet(){
+  addBtn.onclick = () => {
+    window.location.hash = '#/add';
+  };
+}
+// кінець додати новий сет
+
+
+// відміна створення нового сету
+function cancel(){
+  cancelBtn.onclick = function(){
+    count--;
+    localStorage.setItem('idTermsLS', count);
+    exit();
+  }
+}
+// кінець відміна створення нового сету
+
+
+// ф-я конструктор
 function MakeCard(id, name, termsArr, defenetionArr){
   let arrOfTerms = [];
   
@@ -108,24 +288,38 @@ function MakeCard(id, name, termsArr, defenetionArr){
   }
     this.arrOfTerms = arrOfTerms;
 }
+// кінець ф-я конструктор
 
 
+// збереження сету при створенні
 function saveSet(){
   saveTerms.addEventListener('click', () => {
     let nameOfSet = inputName.value;
-    if(nameOfSet !== ''){
-      const terms = document.querySelectorAll('#term');
-      const defenetion = document.querySelectorAll('#defenetion');
-     
+    const terms = document.querySelectorAll('.term');
+    const defenetion = document.querySelectorAll('.defenetion');
+    let booleanCounter = false;
+    let notEmptyCounter = 0;
+
+    terms.forEach((elem, index) => {
+      if(nameOfSet !== '' && (elem.value !== '' || defenetion[index].value !== '')){
+        booleanCounter = true;
+        notEmptyCounter++;
+      }
+    });
+
+    if(booleanCounter && notEmptyCounter === terms.length){
       let mySet = new MakeCard(count, nameOfSet, terms, defenetion);
-      setsArr.push(mySet);
-      localStorage.setItem('setsOfWords', JSON.stringify(setsArr));
-      exit();
-      history.replaceState(null, null, ' ');
+        setsArr.push(mySet);
+        localStorage.setItem('setsOfWords', JSON.stringify(setsArr));
+        exit();
+        history.replaceState(null, null, ' ');
     }
   });
 }
+// кінець збереження сету при створенні
 
+
+// додати новий термін з визначенням
 function addNewTerm(){
   addTerms.addEventListener('click', () => {
     const block = document.createElement('div');
@@ -133,17 +327,17 @@ function addNewTerm(){
 
     const inputTerm = document.createElement('input'),
           inputDefenition = document.createElement('input'),
-          blocks = document.querySelectorAll('.block'),
+          blocks = addPage.querySelectorAll('.block'),
           remove = document.createElement('button');
 
     blocks.forEach(block => {
       block.appendChild(inputTerm).classList.add('input_in_block');
       inputTerm.setAttribute('placeholder', 'enter term');
-      inputTerm.id = 'term';
+      inputTerm.classList.add('term');
 
       block.appendChild(inputDefenition).classList.add('input_in_block');
       inputDefenition.setAttribute('placeholder', 'enter defenition');
-      inputDefenition.id = 'defenetion';
+      inputDefenition.classList.add('defenetion');
 
       block.appendChild(remove).classList.add('remove_term_btn');
       remove.textContent = 'Remove';
@@ -157,22 +351,36 @@ function addNewTerm(){
     });
   });
 }
+// кінець додати новий термін з визначенням
+
+
+function locationHashChanged() {
+  if (location.hash === '#/add') {
+    mainPage.style.display = 'none';
+    editPage.style.display = 'none';
+    addPage.style.display = 'block';
+    count++;
+    addNewSet();
+    cancel();
+    localStorage.setItem('idTermsLS', count);
+  }
+
+  const spans = openTermsBox.querySelectorAll('span');
+  spans.forEach(span => {
+    if(location.hash === `#/modify:item_${setsArr[+span.textContent].id}`){
+      mainPage.style.display = 'none';
+      addPage.style.display = 'none';
+      editPage.style.display = 'block';
+      modify();
+    }
+  });
+  
+}
+
+window.onhashchange = locationHashChanged;
 
 checkLS();
 addNewSet();
 saveSet();
 addNewTerm();
-
-function locationHashChanged() {
-  if (location.hash === '#/add') {
-    
-    // mainPage.style.display = 'none';
-    // addPage.style.display = 'block';
-    addNewSet();
-    // count++;
-    // localStorage.setItem('idTermsLS', count);
-  }
-}
-
-window.onhashchange = locationHashChanged;
-// window.addEventListener('hashchange', locationHashChanged);
+renderStudied();
