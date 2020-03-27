658b2a4f39aaab37eff7c67780c2694710aa15ac
Kostiantyn Marych <54593230+webwork34@users.noreply.github.com>
2020-03-27 17:12:27 +0200

    Delete activePlayer.js

--- FL12_Webinar_HW12_MODULES/src/js/modules/activePlayer.js
+++
@@ -1,9 +0,0 @@
-import objVar from './variables';
-
-export default function activePlayer(){
-  const playerOne = document.querySelector('.player1'),
-        playerTwo = document.querySelector('.player2');
-
-  playerOne.style.textDecoration = objVar.playerOneFirstStep ? 'underline' : 'none';
-  playerTwo.style.textDecoration = objVar.playerOneFirstStep ? 'none' : 'underline';
-}
